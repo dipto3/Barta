078286f9f90220ccf24613b4f00e2300b58300db
dipto <diptoguho4@gmail.com>
2023-11-06 15:14:50 +0600

    partials

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -1,40 +1,19 @@
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
     <meta charset="UTF-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0" />
-    <script src="https://cdn.tailwindcss.com"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @include('frontend.partials.styles')
+    <title>@yield('title', 'Home')</title>
+</head>
 
-    <!-- AlpineJS CDN -->
-    <script
-      defer
-      src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
-    <link
-      rel="preconnect"
-      href="https://fonts.googleapis.com" />
-    <link
-      rel="preconnect"
-      href="https://fonts.gstatic.com"
-      crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-      rel="stylesheet" />
-
-    <style>
-      * {
-        font-family: 'Inter', sans-serif;
-      }
-    </style>
-    <title>@yield('title','Home')</title>
-  </head>
-  <body class="bg-gray-100">
+<body class="bg-gray-100">
     @include('frontend.partials.header')
 
     @yield('main')
 
     @include('frontend.partials.footer')
-  </body>
+</body>
+
 </html>
