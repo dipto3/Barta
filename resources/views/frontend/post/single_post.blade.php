94a465efdddaabacfc2b703101be7c985299df09
dipto <diptoguho4@gmail.com>
2023-11-22 12:08:25 +0600

    profile issue solve

--- resources/views/frontend/post/single_post.blade.php
+++ resources/views/frontend/post/single_post.blade.php
@@ -21,12 +21,12 @@
 
                         <!-- User Info -->
                         <div class="text-gray-900 flex flex-col min-w-0 flex-1">
-                            <a href="{{ url('/profile/user/' . $post->uuid) }}"
+                            <a href="{{ url('/profile/user/' . $post->userUuid) }}"
                                 class="hover:underline font-semibold line-clamp-1">
                                 {{ $post->user_name }}
                             </a>
 
-                            <a href="{{ url('/profile/user/' . $post->uuid) }}"
+                            <a href="{{ url('/profile/user/' . $post->userUuid) }}"
                                 class="hover:underline text-sm text-gray-500 line-clamp-1">
                                 {{ $post->userName }}
                             </a>
