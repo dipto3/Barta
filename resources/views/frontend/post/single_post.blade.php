2507f481454b112c72f84c39e0205722fdaacd75
dipto <diptoguho4@gmail.com>
2023-11-26 15:45:12 +0600

    issue solve

--- resources/views/frontend/post/single_post.blade.php
+++ resources/views/frontend/post/single_post.blade.php
@@ -118,6 +118,7 @@
                                 placeholder="Write a comment..."
                                 class="flex w-full h-auto min-h-[40px] px-3 py-2 text-sm bg-gray-100 focus:bg-white border border-sm rounded-lg border-neutral-300 ring-offset-background placeholder:text-neutral-400 focus:border-neutral-300 focus:outline-none focus:ring-1 focus:ring-offset-0 focus:ring-neutral-400 disabled:cursor-not-allowed disabled:opacity-50 text-gray-900"></textarea>
                         </div>
+                        <input type="hidden" name="postId" value="{{ $post->id }}" />
                     </div>
                 </div>
 
