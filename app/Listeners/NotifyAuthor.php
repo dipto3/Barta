213a50f6dfa5bc4d1b6b47de73aa5befbf1f4e8c
dipto <diptoguho4@gmail.com>
2024-01-06 19:15:07 +0600

    CommentCreated event

---
+++ app/Listeners/NotifyAuthor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\CommentCreated;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class NotifyAuthor
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(CommentCreated $event): void
+    {
+        //
+    }
+}
